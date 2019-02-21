771eaf1f2f1a4dd110cc32f46b75fb5d0e308bcf
Yales Rios <yalesrios@gmail.com>
2019-02-21 15:41:45 +0000

    Bent the code to PEP8's will

--- Source/addition.py
+++ Source/addition.py
@@ -1,17 +1,21 @@
 import random
+
+
 class addition:
 
     def __init__(self):
         pass
 
-    def add(self,varDict):
-       return varDict['Y'] + varDict['X']
-    def generateNumbers(self,varDict):
+    def add(self, varDict):
+        return varDict['Y'] + varDict['X']
+
+    def generateNumbers(self, varDict):
         for i in varDict:
             if (varDict[i] == ""):
-                varDict[i] = random.randint(int(varDict['minbound']), int(varDict['maxbound']))
+                varDict[i] = random.randint(int(varDict['minbound']),
+                                            int(varDict['maxbound']))
         return varDict
-    def generate(self,varDict):
-        varDict = self.generateNumbers(varDict)
-        return(varDict,self.add(varDict))
 
+    def generate(self, varDict):
+        varDict = self.generateNumbers(varDict)
+        return(varDict, self.add(varDict))
--- Source/fileReader.py
+++ Source/fileReader.py
@@ -1,12 +1,16 @@
 import random
 from addition import addition
+import pprint
+
+
 def readFile(fileName):
     # reads in the file, reads every line in the file using readLines
     # RETURNS 'What is $X + $Y equal to?'
-    file = open(fileName, 'r') 
+    file = open(fileName, 'r')
     contentFile = file.readlines()
-    
+
     return contentFile[0]
+
 
 def identifyVariables(question):
     # identifies the variables in the question
@@ -20,16 +24,18 @@ def identifyVariables(question):
             variableName = i.split('$')[1]
             variables.append(variableName)
             dictOfVars[variableName] = ""
-    
+
     if len(question.split("{")) > 1:
         hiddenConstantsString = question.split("{")[1].split("}")[0]
-        for constantDefinitions in  hiddenConstantsString.split(","):
-            dictOfVars[constantDefinitions.split("=")[0]] = constantDefinitions.split("=")[1]
-            
+        for constantDefinitions in hiddenConstantsString.split(","):
+            dictOfVars[constantDefinitions.split("=")[0]] \
+            = constantDefinitions.split("=")[1]
+
     return dictOfVars
 
+
 def writeQuestion(question, answer, dictVariables):
-    # TODO find some way so the user doesnt have to rewrite what the variables are here :)
+    # TODO Make this code work for multiple types of questions
     returnString = []
 
     if len(question.split("{")) > 1:
@@ -44,7 +50,7 @@ def writeQuestion(question, answer, dictVariables):
                 returnString.append(dictVariables['X'])
             if value[1] == "Y":
                 returnString.append(dictVariables['Y'])
-    returnString.append("\n " + str(answer))            
+    returnString.append("\n " + str(answer))
 
     return " ".join(str(x) for x in returnString)
 
@@ -54,8 +60,7 @@ print("readFile is equal to", a)
 
 additionQuestion = addition()
 varDict = additionQuestion.generateNumbers(identifyVariables(a))
-b,c = additionQuestion.generate(identifyVariables(a))
-import pprint
+b, c = additionQuestion.generate(identifyVariables(a))
 pprint.pprint(b)
 print("userChangeVariables is equal to", c)
 
